f3ed1976dbf67d7096ba5920529b12f3d3d6626b
Erik Ogenvik <erik.ogenvik@jayway.com>
2015-09-16 16:06:17 +0200

    Disable button if no user id set.

--- app/scripts/components/createGame/createGame.jsx
+++ app/scripts/components/createGame/createGame.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import GameActions from '../../actions/gameActions'
+import UserStore from '../../stores/userStore'
 import RaisedButton from 'material-ui/lib/raised-button'
 
 class CreateGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.unsubscribe = UserStore.listen(userId => this.setState({
+      userId: userId
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+
   _handleSubmit() {
     GameActions.createGame(this.state.userId);
     return false;
@@ -10,7 +27,7 @@ class CreateGame extends React.Component {
 
   render() {
     return (
-      <RaisedButton label="Create game" onClick={() => GameActions.createGame()}/>
+      <RaisedButton label="Create game" disabled={!this.state.userId} onClick={() => GameActions.createGame()}/>
     );
   }
 }
